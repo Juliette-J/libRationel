a22f26f41c3b715191ec9a12a897d0fce2476212
Ju <juliette.jeannin@hotmail.fr>
2022-12-16 11:22:12 +0100

    make CMake files

---
+++ libRatio/myLib/include/Rational.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+#ifdef RATIONNELS_HPP
+#define RATIONNELS_HPP
+
+class Rationnel {
+    public :
+        Rationnel(const double &a, const double &b);
+        Rationnel(const Rationnel &r);
+
+        ~Rationnel() = default;
+
+    private :
+
+}
+
+#endif
---
+++ libRatio/myLib/src/Rational.cpp
@@ -0,0 +1,5 @@
+#include "..\include\Rational.hpp"
+
+#include <cmath>
+#include <cstdlib>
+
